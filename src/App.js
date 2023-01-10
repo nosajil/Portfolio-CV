550f0588dfad7c77919aa6182d9a9328c94ede90
Jason Lequenne <jsnlequenne@gmail.com>
2023-01-10 11:31:25 +0100

    Changement background étoilé

--- src/App.js
+++ src/App.js
@@ -6,16 +6,39 @@ import SecondPage from './components/SecondPage';
 import './sass/App.scss';
 import Contact from './components/Contact';
 import { BrowserRouter } from 'react-router-dom';
+import Particles from 'particlesjs';
 
 const App = () => {
+  window.onload = function() {Particles.init({
+    selector: '.background',
+    speed: .1,
+    sizeVariations: 1,
+    maxParticles: 10000,
+    connectParticules: true,
+
+    // color: ['#1B1464', '#404B69', '#5758BB'],
+    color: ['#ffffff', '#898a8d', '#5758BB'],
+    responsive: [{
+      breakpoint: 768,
+      options: {
+        color: ['#ffffff', '#898a8d', '#5758BB'],
+        connectParticules: true,
+        maxParticles: 2500,
+      }
+    }]
+    
+
+  });
+};
+
   return (
     <BrowserRouter>
       <div className="App">
           <Nav />
           <div className="body-wrapper">
-              <video autoPlay muted loop id="myVideo">
+              {/* <video autoPlay muted loop id="myVideo">
                 <source src={`${process.env.PUBLIC_URL}/video/background_video2.mp4`}/>
-              </video>
+              </video> */}
             <FirstPage />
             <Presentation />
             <SecondPage />
@@ -24,6 +47,7 @@ const App = () => {
           </div>
       </div>
     </BrowserRouter>
+    
   );
 }
 
